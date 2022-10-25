fb8c48ee426a9ee8cc27bb4d5c77fddc2fbfafbd
svCcare <maciej.pawlak94@gmail.com>
2022-10-25 23:32:19 +0200

    Added test for Day1.2

--- Day01-NotQuiteLisp.Tests/ElevatorLogicTest.cs
+++ Day01-NotQuiteLisp.Tests/ElevatorLogicTest.cs
@@ -4,13 +4,13 @@ namespace Day01_NotQuiteLisp.Tests
 {
     public class ElevatorLogicTest
     {
-        const char CharacterUp = ')';
-        const char CharacterDown = '(';
+        const char CharacterUp = '(';
+        const char CharacterDown = ')';
 
         [Theory]
         [InlineData("(())", CharacterUp, CharacterDown, 0)]
         [InlineData("(()(()(", CharacterUp, CharacterDown, 3)]
-        [InlineData("))(((((", CharacterUp, CharacterDown, 3)]
+        [InlineData("))((((((", CharacterUp, CharacterDown, 4)]
         [InlineData("())", CharacterUp, CharacterDown, -1)]
         [InlineData(")())())", CharacterUp, CharacterDown, -3)]
         public void SolveOneGivesExpectedOutput(string testInput, char characterUp, char characterDown, int expected)
@@ -20,6 +20,21 @@ namespace Day01_NotQuiteLisp.Tests
 
             // Act
             var result = elevatorLogic.SolveOne();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(")", CharacterUp, CharacterDown, 1)]
+        [InlineData("()())", CharacterUp, CharacterDown, 5)]
+        public void SolveTwoGivesExpectedOutput(string testInput, char characterUp, char characterDown, int expected)
+        {
+            // Arrange
+            var elevatorLogic = new ElevatorLogic(testInput, characterUp, characterDown);
+
+            // Act
+            var result = elevatorLogic.SolveTwo();
 
             // Assert
             Assert.Equal(expected, result);
