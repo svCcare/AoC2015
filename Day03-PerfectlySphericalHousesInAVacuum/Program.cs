4d5b57cb68191a93e5c29fe2cb782313d0339b7d
svCcare <maciej.pawlak94@gmail.com>
2022-10-31 21:47:23 +0100

    Day 03.01 solved

---
+++ Day03-PerfectlySphericalHousesInAVacuum/Program.cs
@@ -0,0 +1,19 @@
+﻿using SharedTools;
+
+namespace Day03_PerfectlySphericalHousesInAVacuum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileReader = new FileReader("input.txt", ReadOption.All);
+
+            SantaDelivery santaDelivery = new SantaDelivery(fileReader.Text);
+            santaDelivery.BeginDelivery();
+
+            Console.WriteLine($"Part 1: {santaDelivery.GetAddressCheckListCount()}");
+
+        }
+
+    }
+}
---
+++ Day03-PerfectlySphericalHousesInAVacuum/SantaDelivery.cs
@@ -0,0 +1,57 @@
+﻿using System.Drawing;
+
+namespace Day03_PerfectlySphericalHousesInAVacuum
+{
+    internal class SantaDelivery
+    {
+        private Dictionary<Point, int> _addressCheckList = new();
+        private readonly string _directions;
+        private Point _currentLocation = new Point(0, 0);
+
+        internal SantaDelivery(string directions)
+        {
+            _directions = directions;
+            _addressCheckList.Add(_currentLocation, 1);
+        }
+
+        internal void BeginDelivery()
+        {
+            foreach (var directionChar in _directions)
+            {
+                switch (directionChar)
+                {
+                    case '>':
+                        _currentLocation.X++;
+                        break;
+                    case '<':
+                        _currentLocation.X--;
+                        break;
+                    case '^':
+                        _currentLocation.Y++;
+                        break;
+                    case 'v':
+                        _currentLocation.Y--;
+                        break;
+                }
+
+                AddToAddressCheckList(_currentLocation);
+            }
+        }
+
+        internal int GetAddressCheckListCount()
+        {
+            return _addressCheckList.Count;
+        }
+
+        private void AddToAddressCheckList(Point currentLocation)
+        {
+            if (!_addressCheckList.ContainsKey(currentLocation))
+            {
+                _addressCheckList.Add(currentLocation, 1);
+                return;
+            }
+
+            _addressCheckList[currentLocation]++;
+        }
+    }
+}
