ef1a40134c43387060657d2a591b10898ac51671
svCcare <maciej.pawlak94@gmail.com>
2022-11-02 22:24:17 +0100

    Day 4.1 and 4.2 solutions

---
+++ Day04-TheIdealStockingStuffer/MD5Solver.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace Day04_TheIdealStockingStuffer
+{
+    internal class MD5Solver
+    {
+        private readonly string input;
+
+        internal MD5Solver(string input)
+        {
+            this.input = input;
+        }
+
+        internal (int iterations, long elapsed) Solve(bool secondPart = false)
+        {
+            int counter = 0;
+            string currentKey = input + counter;
+
+            var timer = new Stopwatch();
+            timer.Start();
+
+            using (MD5 md5Hash = MD5.Create())
+            {
+                while (IncrementNeeded(md5Hash, currentKey, secondPart))
+                {
+                    counter++;
+                    currentKey = input + counter;
+                }
+            }
+
+            timer.Stop();
+
+            return (counter, timer.ElapsedMilliseconds);
+        }
+
+        private bool IncrementNeeded(MD5 md5Hash, string currentKey, bool secondPart)
+        {
+            byte[] hashBytes = md5Hash.ComputeHash(System.Text.Encoding.ASCII.GetBytes(currentKey));
+
+            if (hashBytes[0] != 0x00)
+            {
+                return true;
+            }
+
+            if (hashBytes[1] != 0x00)
+            {
+                return true;
+            }
+
+            if (hashBytes[2] > 0x0F)
+            {
+                return true;
+            }
+
+            if (secondPart && hashBytes[2] > 0x00)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Day04-TheIdealStockingStuffer/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day04_TheIdealStockingStuffer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MD5Solver md5solver = new("ckczppom");
+
+            var part1 = md5solver.Solve();
+            var part2 = md5solver.Solve(true);
+
+            Console.WriteLine($"Part 1: {part1.iterations}, found in: {part1.elapsed}ms");
+            Console.WriteLine($"Part 2: {part2.iterations}, found in: {part2.elapsed}ms");
+        }
+
+    }
+}
