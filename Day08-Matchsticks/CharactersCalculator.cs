3d71a897bc4ae8c03003ef66c2178994d28c8b94
svCcare <maciej.pawlak94@gmail.com>
2022-11-07 19:20:44 +0100

    Day 8.1 solution

---
+++ Day08-Matchsticks/CharactersCalculator.cs
@@ -0,0 +1,57 @@
+﻿namespace Day08_Matchsticks
+{
+    internal class CharactersCalculator
+    {
+        char escapeChar = '\\';
+        string hexChar = "\\x";
+
+        public List<(int physicalCount, int logicalCount)> Characters { get; set; } = new();
+
+        internal void Calculate(string line)
+        {
+            var physical = line.Length;
+
+            var logical = CalculateLogicalLength(line);
+
+            Characters.Add((physical, logical));
+        }
+
+        internal int CalculateLogicalLength(string line) // "\xa8br\x8bjr\""
+        {
+            int lineLenght = 0;
+            line = line.Substring(1, line.Length - 2); // removing " " wrappers
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == escapeChar)
+                {
+                    if (line[i].ToString() + line[i + 1] == hexChar)
+                    { // its \x case
+                        i += 3;
+                        lineLenght++;
+                    }
+                    else
+                    {
+                        i += 1;
+                        lineLenght++;
+                    }
+                }
+                else
+                {
+                    lineLenght++;
+                }
+            }
+
+            // \xa8br\x8bjr\"
+            // \xa8 = 1 logical
+            // b
+            // r
+            // \x8b = 1 logical
+            // j
+            // r
+            // \" = 1 logical
+
+            return lineLenght;
+        }
+    }
+}
---
+++ Day08-Matchsticks/Program.cs
@@ -0,0 +1,23 @@
+﻿using SharedTools;
+
+namespace Day08_Matchsticks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileReader = new FileReader("input.txt", ReadOption.Lines);
+
+            CharactersCalculator charactersCalculator = new CharactersCalculator();
+
+            foreach (var line in fileReader.TextLines)
+            {
+                charactersCalculator.Calculate(line);
+            }
+
+            var result = charactersCalculator.Characters.Select(x => x.physicalCount - x.logicalCount).Sum();
+
+            Console.WriteLine($"Part 1: ");
+        }
+    }
+}
