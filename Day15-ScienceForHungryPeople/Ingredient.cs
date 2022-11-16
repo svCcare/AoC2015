30df565ccd3430b6d16d1b896c86da32a2bbdd71
svCcare <maciej.pawlak94@gmail.com>
2022-11-16 17:44:52 +0100

    Day 15 solution

---
+++ Day15-ScienceForHungryPeople/Ingredient.cs
@@ -0,0 +1,23 @@
+﻿namespace Day15_ScienceForHungryPeople
+{
+    internal class Ingredient
+    {
+        public string Name { get; }
+        public int Capacity { get; }
+        public int Durability { get; }
+        public int Flavor { get; }
+        public int Texture { get; }
+        public int Calories { get; }
+
+        public Ingredient(string input)
+        {
+            var parts = input.Split(' ');
+            Name = parts[0].Replace(":", string.Empty);
+            Capacity = Convert.ToInt32(parts[2].Replace(",", string.Empty));
+            Durability = Convert.ToInt32(parts[4].Replace(",", string.Empty));
+            Flavor = Convert.ToInt32(parts[6].Replace(",", string.Empty));
+            Texture = Convert.ToInt32(parts[8].Replace(",", string.Empty));
+            Calories = Convert.ToInt32(parts[10].Replace(",", string.Empty));
+        }
+    }
+}
---
+++ Day15-ScienceForHungryPeople/Program.cs
@@ -0,0 +1,83 @@
+﻿using SharedTools;
+using System.Diagnostics;
+
+namespace Day15_ScienceForHungryPeople
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            var ingredients = new FileReader("input.txt", ReadOption.Lines).TextLines.Select(x => new Ingredient(x)).ToList();
+
+            int[] possibleQuantities = new int[ingredients.Count];
+            Dictionary<string, int> combinations = new Dictionary<string, int>();
+
+            do
+            {
+                for (int i = 0; i < ingredients.Count; i++)
+                {
+                    possibleQuantities[i]++;
+                    if (possibleQuantities[i] > 100)
+                    {
+                        possibleQuantities[i] = 0;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (possibleQuantities.Sum() != 100)
+                {
+                    continue;
+                }
+
+                // create combination
+                var combinationKey = string.Join(";", possibleQuantities);
+
+                if (combinations.ContainsKey(combinationKey))
+                {
+                    break;
+                }
+
+                int score = CalculateScore(ingredients, possibleQuantities);
+                if (score < 0)
+                {
+                    combinationKey += "!";
+                    score *= -1;
+                }
+                combinations.Add(combinationKey, score);
+
+            } while (true);
+
+            var bestScore = combinations.OrderByDescending(x => x.Value).First();
+
+            Console.WriteLine($"Winning score {bestScore.Value} | elapsed: {timer.ElapsedMilliseconds}ms");
+        }
+
+        private static int CalculateScore(List<Ingredient> ingredientsF, int[] possibleQuantities)
+        {
+            int capacity = 0;
+            int durability = 0;
+            int flavor = 0;
+            int texture = 0;
+            int calories = 0;
+
+            for (int i = 0; i < possibleQuantities.Length; i++)
+            {
+                capacity += ingredientsF[i].Capacity * possibleQuantities[i];
+                durability += ingredientsF[i].Durability * possibleQuantities[i];
+                flavor += ingredientsF[i].Flavor * possibleQuantities[i];
+                texture += ingredientsF[i].Texture * possibleQuantities[i];
+                calories += ingredientsF[i].Calories * possibleQuantities[i];
+            }
+
+
+            var score = calories != 500 ? 0 : Math.Max(0, capacity) * Math.Max(0, durability) * Math.Max(0, flavor) * Math.Max(0, texture);
+            return score;
+        }
+    }
+}
