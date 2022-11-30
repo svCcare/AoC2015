461bff61e194edbe40e3a4f1a143c2591152f957
MACPA <macpa@orsted.dk>
2022-11-30 13:38:11 +0100

    Day 20.1 20.2 solution

--- Day20-InfiniteElvesAndInfiniteHouses/Program.cs
+++ Day20-InfiniteElvesAndInfiniteHouses/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+namespace Day20_InfiniteElvesAndInfiniteHouses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = 0;
+            var houseIndex = 0;
+            var presentThreshold = 34000000;
+
+            do
+            {
+                result = 0;
+                houseIndex++;
+                var elfNumbers = CalculateFactors(houseIndex);
+                var workingElves = elfNumbers.Where(x => x * 50 >= houseIndex);
+
+                foreach (var elfNumber in workingElves)
+                {
+                    result += elfNumber * 11;
+                }
+
+            } while (result < presentThreshold);
+
+            Console.WriteLine($"House index {houseIndex}");
+        }
+
+        public static List<int> CalculateFactors(int number)
+        {
+            var factors = new List<int>();
+            int max = (int)Math.Sqrt(number);  // Round down
+
+            for (int factor = 1; factor <= max; ++factor) // Test from 1 to the square root, or the int below it, inclusive.
+            {
+                if (number % factor == 0)
+                {
+                    factors.Add(factor);
+                    if (factor != number / factor) // Don't add the square root twice!  Thanks Jon
+                        factors.Add(number / factor);
+                }
+            }
+            return factors;
+        }
+    }
+}
